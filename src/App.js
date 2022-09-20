617cc330f406fc258723c22288272289db4c12de
Damian Ramanzin <damianramanzin@gmail.com>
2022-09-20 14:44:32 -0300

    botón de cantidades agregado

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import './App.css';
 import NavBar from './components/Navbar/NavBar';
 import CartWidget from './components/Navbar/CartWidget';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import ItemCount from './components/ItemCount/ItemCount';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <CartWidget />
       <ItemListContainer greeting="¡Bienvenido a Librosonline!" />
+      <ItemCount/>
     </div>
   );
 }
